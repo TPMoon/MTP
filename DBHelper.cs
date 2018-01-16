ee1cfbde29b0c79ca076c95055dc8f9661977ddb
TPMoon <2408289544@qq.com>
2018-01-16 19:05:18 +0800

    DBHelper.cs

---
+++ DBHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Frm_KY
+{
+    class DBHelper
+    {
+        private SqlConnection con;
+        public SqlConnection Con {
+            get {
+                string Sqlcon = "Data Source=.;Initial Catalog=马天鹏;User ID=sa;Pwd=.";
+                if (con == null)
+                {
+                    con = new SqlConnection(Sqlcon);
+                }
+                return con;
+            }
+        }
+        public void Oper() {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }else if(con.State==ConnectionState.Broken){
+                con.Close();
+                con.Open();
+            }
+        }
+        public void Close() { 
+            if(con.State==ConnectionState.Closed||con.State==ConnectionState.Broken){
+                con.Close();
+            }
+        }
+    }
+}
